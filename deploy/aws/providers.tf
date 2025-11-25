ee6a91012c4b6b77035d47961af0647cfd6c2a94
keeonline <keeonline@outlook.com>
2025-11-25 17:15:53 +0000

    updated deployment workflow

---
+++ deploy/aws/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.87.0"
+    }
+  }
+}
+
+provider "aws" {
+    default_tags {
+      tags = {
+        AppEnvironment = "${var.app_environment}"
+        IacVersion = "${var.app_version}"
+        IacRepo = "${var.app_repo}"
+        InfraEnvironment = "${var.infra_environment}"
+      }
+    }
+}
---
+++ deploy/aws/variables.tf
@@ -0,0 +1,15 @@
+variable "app_environment" {
+    type = string
+} 
+
+variable "app_version" {
+    type = string
+}
+
+variable "app_repo" {
+    type = string
+}
+
+variable "infra_environment" {
+    type = string
+}
