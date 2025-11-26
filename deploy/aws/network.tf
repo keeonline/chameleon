b8d2c0af9f4cd821c052da4337cdcb8339a3bb63
keeonline <keeonline@outlook.com>
2025-11-26 13:12:10 +0000

    added dependencies to deploy code

---
+++ deploy/aws/network.tf
@@ -0,0 +1,40 @@
+data "aws_vpc" "main" {
+    filter {
+        name = "tag:InfraEnvironment"
+        values = ["${var.infra_environment}"]
+    }
+}
+
+data "aws_subnets" "public" {
+    filter {
+        name = "tag:InfraEnvironment"
+        values = ["${var.infra_environment}"]        
+    }
+  
+    filter {
+        name = "tag:Visibility"
+        values = ["public"]        
+    }
+}
+
+data "aws_subnet" "public" {
+    for_each = toset(data.aws_subnets.public.ids)
+    id = each.value
+}
+
+data "aws_subnets" "private" {
+    filter {
+        name = "tag:InfraEnvironment"
+        values = ["${var.infra_environment}"]        
+    }
+  
+    filter {
+        name = "tag:Visibility"
+        values = ["private"]        
+    }
+}
+
+data "aws_subnet" "private" {
+    for_each = toset(data.aws_subnets.private.ids)
+    id = each.value
+}
